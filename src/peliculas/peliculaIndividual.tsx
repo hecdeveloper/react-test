cdd0df951ce033e3a2cfb4b01f12fa933f69e3e1
Hector Ibarra <hibarra@imaginuity.com>
2022-12-16 16:14:57 -0600

    interface

---
+++ src/peliculas/peliculaIndividual.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const peliculaIndividual = () => {
+  return (
+    <div>peliculaIndividual</div>
+  )
+}
+
+export default peliculaIndividual
+
+interface peliculaIndividualProps {
+    pelicula: any;
+}
