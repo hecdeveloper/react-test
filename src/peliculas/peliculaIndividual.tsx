2c1995e3636dfda7867275380770f3cd156295bc
Hector Ibarra <hibarra@imaginuity.com>
2022-12-16 16:22:48 -0600

    cosilla

--- src/peliculas/peliculaIndividual.tsx
+++ src/peliculas/peliculaIndividual.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { pelicula } from './peliculas.model';
+import React from "react";
+import { pelicula } from "./peliculas.model";
 
 const peliculaIndividual = (props: peliculaIndividualProps) => {
+  const construirLink = () => `/pelicula/${props.pelicula.id}`;
   return (
-    <div>peliculaIndividual</div>
-  )
-}
+    <div>
+      <a href={construirLink()}>
+        <img src={props.pelicula.poster} alt="POSTER" />
+      </a>
+      <p>
+        <a href={construirLink()}>{props.pelicula.titulo}</a>
+      </p>
+    </div>
+  );
+};
 
-export default peliculaIndividual
+export default peliculaIndividual;
 
 interface peliculaIndividualProps {
-    pelicula: pelicula;
+  pelicula: pelicula;
 }
