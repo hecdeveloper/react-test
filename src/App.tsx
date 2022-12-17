2c6cb5c12d1bb1395dfea7e017769b6fe9fb794a
Hector Ibarra <hibarra@imaginuity.com>
2022-12-17 01:10:33 -0600

    condicionales peliculas

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ListadoPeliculas from "./peliculas/ListadoPeliculas";
 
-import { pelicula } from "./peliculas/peliculas.model";
+import { landingPageDTO } from "./peliculas/peliculas.model";
 function App() {
-
-  useEffect(() =>{
+  const [peliculas, setPeliculas] = useState<landingPageDTO>({});
+  useEffect(() => {
     const timerId = setTimeout(() => {
-      
-    
+      setPeliculas({
+        enCartelera:[
+          {
+            id: 1,
+            titulo: "Spiderman",
+            poster:
+              "https://cdn.shopify.com/s/files/1/0265/2769/4934/products/spiderman_ver1_xxlg_1024x1024@2x.jpg?v=1628007516",
+          },
+          {
+            id: 2,
+            titulo: "Moana",
+            poster: "https://m.media-amazon.com/images/I/618d8ue5SmL._AC_.jpg",
+          },
+        ],
+        proximosEstrenos:[
+          {
+            id: 3,
+            titulo: "Soul",
+            poster:
+              "https://lumiere-a.akamaihd.net/v1/images/p_soul_disneyplus_v2_20907_764da65d.jpeg",
+          },
+        ]
+      })
     }, 1000);
     return () => clearTimeout(timerId);
-  })
-  const peliculasEnCartelera: pelicula[] = [
-    {
-      id: 1,
-      titulo: "Spiderman",
-      poster:
-        "https://cdn.shopify.com/s/files/1/0265/2769/4934/products/spiderman_ver1_xxlg_1024x1024@2x.jpg?v=1628007516",
-    },
-    {
-      id: 2,
-      titulo: "Moana",
-      poster:
-        "https://m.media-amazon.com/images/I/618d8ue5SmL._AC_.jpg",
-    },
-  ]
-  const peliculasProximosEstrenos: pelicula[] = [
-    {
-      id: 3,
-      titulo: "Soul",
-      poster:
-        "https://lumiere-a.akamaihd.net/v1/images/p_soul_disneyplus_v2_20907_764da65d.jpeg",
-    },
+  });
 
-  ]
-  // const peliculaPrueba: pelicula = {
-  //   id: 1,
-  //   titulo: "Spiderman",
-  //   poster:
-  //     "https://cdn.shopify.com/s/files/1/0265/2769/4934/products/spiderman_ver1_xxlg_1024x1024@2x.jpg?v=1628007516",
-  // };
+
   return (
     <>
       {/* <PeliculaIndividual pelicula={peliculaPrueba} /> */}
       <h3>En Cartelera</h3>
-      <ListadoPeliculas peliculas={peliculasEnCartelera}/>
+      <ListadoPeliculas peliculas={peliculas.enCartelera} />
       <h3>Estrenos</h3>
-      <ListadoPeliculas peliculas={peliculasProximosEstrenos}/>
+      <ListadoPeliculas peliculas={peliculas.proximosEstrenos } />
     </>
   );
 }
--- src/peliculas/ListadoPeliculas.tsx
+++ src/peliculas/ListadoPeliculas.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import PeliculaIndividual from "./PeliculaIndividual";
 import { pelicula } from "./peliculas.model";
-import css from './ListadoPeliculas.module.css'
+import css from "./ListadoPeliculas.module.css";
 
 const ListadoPeliculas = (props: listadoPelliculasProps) => {
-  return (
-    <div className={css.div}>
-      {props.peliculas.map((pelicula) => (
-        <PeliculaIndividual pelicula={pelicula} key={pelicula.id} />
-      ))}
-    </div>
-  );
+  if(!props.peliculas){
+    return <></>
+  }else if (props.peliculas.length === 0) {
+    return <p>No hay peliculas</p>;
+  }else {
+    return(
+      <div className={css.div}>
+        {props.peliculas.map((pelicula) => (
+          <PeliculaIndividual pelicula={pelicula} key={pelicula.id} />
+        ))}
+      </div>
+    )
+  }
 };
 
 export default ListadoPeliculas;
 
 interface listadoPelliculasProps {
-  peliculas: pelicula[];
+  peliculas?: pelicula[];
 }
