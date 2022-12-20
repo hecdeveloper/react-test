0f0b8968f095e27128dae63882ec8c2c69e8954c
Hector Ibarra <hibarra@imaginuity.com>
2022-12-20 15:52:33 -0600

    cosas validaciones

---
+++ src/utils/ListadoGenerico.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from 'react'
+import Cargando from './Cargando'
+
+const ListadoGenerico = (props:listadoGenericoProps) => {
+    if(!props.listado){
+        if(props.cargandoUI){
+            return props.cargandoUI
+        }
+        return <Cargando/>
+    }else if(props.listado.listado.length === 0){
+        if(props.listadoVacioUI){
+            return props.listadoVacioUI
+        }
+        return <p>No hay elementos mara mostrar</p>
+    }else {
+        return props.children;
+    }
+}
+
+export default ListadoGenerico
+interface listadoGenericoProps {
+    listado: any;
+    children: ReactElement; 
+    cargandoUI?: ReactElement;
+    listadoVacioUI?: ReactElement;
+}
