c58e992da8005e5d11a18984f9e087d31da89587
Hector Ibarra <hibarra@imaginuity.com>
2022-12-20 19:47:28 -0600

    clase activa perrona

--- src/utils/Menu.tsx
+++ src/utils/Menu.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 const Menu = () => {
+  const claseActiva = "active";
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <NavLink to="/" className="navbar-brand">React Peliculas</NavLink>
+        <NavLink activeClassName={claseActiva} to="/" className="navbar-brand">
+          React Peliculas
+        </NavLink>
         <div className="collapse navbar-collapse">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/generos">
-                        Generos
-                    </NavLink>
-                </li>
-            </ul>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                activeClassName={claseActiva}
+                className="nav-link"
+                to="/generos"
+              >
+                Generos
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
