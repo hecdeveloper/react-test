768e377c2af9d8cbb024a426d5c6e5077c96d0cd
Hector Ibarra <hibarra@imaginuity.com>
2022-12-20 19:40:32 -0600

    landing

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,7 @@ import { landingPageDTO } from "./peliculas/peliculas.model";
 import Menu from "./utils/Menu";
 import IndiceGeneros from "./generos/IndiceGeneros";
 function App() {
-  const [peliculas, setPeliculas] = useState<landingPageDTO>({});
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setPeliculas({
-        enCartelera: [
-          {
-            id: 1,
-            titulo: "Spiderman",
-            poster:
-              "https://cdn.shopify.com/s/files/1/0265/2769/4934/products/spiderman_ver1_xxlg_1024x1024@2x.jpg?v=1628007516",
-          },
-          {
-            id: 2,
-            titulo: "Moana",
-            poster: "https://m.media-amazon.com/images/I/618d8ue5SmL._AC_.jpg",
-          },
-        ],
-        proximosEstrenos: [
-          {
-            id: 3,
-            titulo: "Soul",
-            poster:
-              "https://lumiere-a.akamaihd.net/v1/images/p_soul_disneyplus_v2_20907_764da65d.jpeg",
-          },
-        ],
-      });
-    }, 500);
-    return () => clearTimeout(timerId);
-  });
+
 
   return (
     <><BrowserRouter>
@@ -43,10 +15,7 @@ function App() {
         
           <Switch>
             <Route exact path="/">
-              <h3>En Cartelera</h3>
-              <ListadoPeliculas peliculas={peliculas.enCartelera} />
-              <h3>Estrenos</h3>
-              <ListadoPeliculas peliculas={peliculas.proximosEstrenos} />
+         
             </Route>
             <Route path="/generos">
             <IndiceGeneros/>
---
+++ src/LandingPage.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import ListadoPeliculas from "./peliculas/ListadoPeliculas";
+import { landingPageDTO } from "./peliculas/peliculas.model";
+
+const LandingPage = () => {
+  const [peliculas, setPeliculas] = useState<landingPageDTO>({});
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setPeliculas({
+        enCartelera: [
+          {
+            id: 1,
+            titulo: "Spiderman",
+            poster:
+              "https://cdn.shopify.com/s/files/1/0265/2769/4934/products/spiderman_ver1_xxlg_1024x1024@2x.jpg?v=1628007516",
+          },
+          {
+            id: 2,
+            titulo: "Moana",
+            poster: "https://m.media-amazon.com/images/I/618d8ue5SmL._AC_.jpg",
+          },
+        ],
+        proximosEstrenos: [
+          {
+            id: 3,
+            titulo: "Soul",
+            poster:
+              "https://lumiere-a.akamaihd.net/v1/images/p_soul_disneyplus_v2_20907_764da65d.jpeg",
+          },
+        ],
+      });
+    }, 500);
+    return () => clearTimeout(timerId);
+  });
+  return (
+    <>
+      <h3>En Cartelera</h3>
+      <ListadoPeliculas peliculas={peliculas.enCartelera} />
+      <h3>Estrenos</h3>
+      <ListadoPeliculas peliculas={peliculas.proximosEstrenos} />
+    </>
+  );
+};
+
+export default LandingPage;
